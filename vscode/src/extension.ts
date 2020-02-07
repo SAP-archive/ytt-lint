c36b9f81054c12cb0420b456a6f653b5990c39e3
Philipp Stehle <philipp.stehle@sap.com>
2020-02-07 10:51:59 +0100

    execute the linter

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as child_process from 'child_process';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,6 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ytt-lint" is now active!');
+	let timeout: NodeJS.Timer | undefined = undefined;
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -18,9 +20,63 @@ export function activate(context: vscode.ExtensionContext) {
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World!');
+		//vscode.window.showInformationMessage(`activeEditor.document.languageId: ${activeEditor.document.languageId}`);
+
 	});
 
+	let diagnosticCollection = vscode.languages.createDiagnosticCollection();
+
 	context.subscriptions.push(disposable);
+
+	function lint() {
+		if (activeEditor == null) {
+			return;
+		}
+		if (activeEditor.document.languageId != "yaml") {
+			return;
+		}
+
+		// TODO: don't use '-f -' if file is saved
+		let yaml = activeEditor.document.getText();
+
+		vscode.window.showInformationMessage('Running lint now!');
+
+		// TODO: use spwan and then stream
+		let linter = child_process.execFile('/home/d060677/go/src/github.com/k14s/ytt/ytt-lint', ['-f', '-', '-o', 'json'], (error, stdout, stderr) => {
+			//JSON.parse(stdout);
+			vscode.window.showInformationMessage(stdout);
+
+		});
+		linter.stdin?.write(yaml);
+		linter.stdin?.end();
+	}
+
+	function triggerUpdateDecorations() {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = undefined;
+		}
+		timeout = setTimeout(lint, 500);
+	}
+
+	let activeEditor = vscode.window.activeTextEditor;
+
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
+
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
 }
 
 // this method is called when your extension is deactivated

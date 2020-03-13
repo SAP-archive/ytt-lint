c32ca73476650bb710a9ccd779ec9d5fb41cfeee
Philipp Stehle <philipp.stehle@sap.com>
2020-03-13 15:00:07 +0100

    Strip debug symbols from executables

--- build-all.sh
+++ build-all.sh
@@ -2,6 +2,6 @@
 
 mkdir -p out
 
-GOOS=linux   go build -o out/ytt-lint-linux  ./cmd/ytt-lint/
-GOOS=darwin  go build -o out/ytt-lint-darwin ./cmd/ytt-lint/
-GOOS=windows go build -o out/ytt-lint-win32  ./cmd/ytt-lint/
+GOOS=linux   go build -ldflags="-s -w" -o out/ytt-lint-linux  ./cmd/ytt-lint/
+GOOS=darwin  go build -ldflags="-s -w" -o out/ytt-lint-darwin ./cmd/ytt-lint/
+GOOS=windows go build -ldflags="-s -w" -o out/ytt-lint-win32  ./cmd/ytt-lint/

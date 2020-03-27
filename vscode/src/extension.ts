dcc8e08485bf2f36a6d0100e132324c56e65ed03
Philipp Stehle <philipp.stehle@sap.com>
2020-03-27 11:05:27 +0100

    use OutputChannel in vscode for pull

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -23,8 +23,22 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.pullSchemaK8S', () => {
-		let terminal = vscode.window.createTerminal(`ytt-lint schema pull`, EXEC_PATH, ['--pull-from-k8s']);
-		terminal.show();
+		let out = vscode.window.createOutputChannel(`ytt-lint schema pull`);
+		let exec = child_process.execFile(EXEC_PATH, ['--pull-from-k8s']);
+
+		exec.stdout?.on('data', (data) => {
+			out.append(data);
+		});
+
+		exec.stderr?.on('data', (data) => {
+			out.append(data);
+		});
+
+		exec.on('close', (code) => {
+			out.appendLine(`Exit code: ${code}`);
+		});
+
+		out.show();
 	});
 
 	let diagnosticCollection = vscode.languages.createDiagnosticCollection('ytt-lint');
